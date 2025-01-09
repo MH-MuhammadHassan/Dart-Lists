9db103e521a7020798a73bf36e873b5ba16c3059
HassanCodeWave <shforshaikhhassan@gmail.com>
2025-01-09 23:44:02 +0500

    format updated

--- reduce_method.dart
+++ reduce_method.dart
@@ -1,15 +1,15 @@
 // reduce: Reduces the list to a single value.
 void main() {
-  // List<int> numbers = [1, 2, 3, 4, 5];
-  // var sum = numbers.reduce((a, b) => a + b); // add all element
-  // print(sum); // Output: 15
+  List<num> numbers = [1, 2, 3, 4, 5];
+  var sum = numbers.reduce((a, b) => a + b);
+  print(sum);
 
-  // Q.4: Create a list of numbers & write a program to get the smallest & greatest
+// Q: Create a list of numbers & write a program to get the smallest & greatest
 // number from a list.
 
   List<num> numbers1 = [2, 3, 1, 22, 56, 101, 53, 66, 500];
-  final smallestValue = numbers1.reduce((min, max) => min < max ? min : max);
-  final greatestValue = numbers1.reduce((min, max) => min > max ? min : max);
+  num smallestValue = numbers1.reduce((min, max) => min < max ? min : max);
+  num greatestValue = numbers1.reduce((min, max) => min > max ? min : max);
 
   print(smallestValue);
   print(greatestValue);

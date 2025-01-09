e2c2a7d597c7e623ba48b18cf07cc939f0a7e853
HassanCodeWave <shforshaikhhassan@gmail.com>
2025-01-09 23:42:46 +0500

    important examples added

--- examples.dart
+++ examples.dart
@@ -2,19 +2,58 @@ import "dart:io";
 
 // 💨💨💨 Nesting Lists With Maps 💨💨💨
 void main() {
-  dynamic abc = [
-    [3, 5, 6],
-    {
-      "hello": {
-        2: ["abc", 2, 3],
-        1: {
-          "1": 0, // access this 0
-          1: "Access this Line", // print(abc[1]["hello"][1][1]);
-        },
-      }
-    },
-    ["1"],
-  ];
-  print(abc[1]["hello"][1]["1"]); // access 0 of line 10
-  print(abc[1]["hello"][1][1]); // print "Access this Line" of line 11
+  // 💨💨💨 Write a program that takes a list of numbers from user and returns the sum of all even numbers.
+  // print("Use ',' to separte values ");
+  // stdout.write("Enter The Numbers List: ");
+  // List<int> numbers = [];
+  // numbers = stdin.readLineSync()!.split(',').map(int.parse).toList();
+  // print('User Entered List: $numbers');
+  // var even = numbers.where((a) => a % 2 == 0).toList();
+  // print("List Of Even Numbers: $even");
+  // var sum = even.reduce((a, b) => a + b);
+  // print("Sum of the Even Number list is: $sum");
+
+  // 💨💨💨 Create a list of integers from 1 to 10 and filter out odd numbers into a new list.
+  // List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  // var odd = numbers.where((test) => test % 2 != 0).toList();
+  // print("Orignal List: $numbers");
+  // print("Odd Number List: $odd");
+
+  // 💨💨💨 Write a program to find the second largest number in a list.
+  // List<int> numbers = [20, 1, 2, 3, 284, 5];
+  // var sortedList = List.from(numbers)..sort();
+  // var secondLargest = sortedList[sortedList.length - 2];
+  // print("Orignal List: $numbers");
+  // print("Sorted List: $sortedList");
+  // print("Second Largest Element Of List is: $secondLargest");
+
+  // 💨💨💨 Merge two lists, remove duplicates, and sort the result in descending order.
+  // List<int> List1 = [3, 2, 1];
+  // List<int> List2 = [1, 2, 3, 4, 5];
+  // var mergeLists = List1 + List2;
+  // print("Merge List: $mergeLists");
+  // var removeDuplicates = mergeLists.toSet().toList();
+  // print("Remove Duplicates: $removeDuplicates");
+  // var sortList = List.from(removeDuplicates..sort());
+  // print("Sorted List: $sortList");
+
+  // 💨💨💨 Implement a program that checks if a given list is a palindrome.
+  // List<int> listOne = [1, 2, 3, 2, 1];
+  // var reverseList = List.from(listOne.reversed); // Reverse the list
+  // print('Reversed List: $reverseList');
+
+  // // Compare each element of listOne with reverseList
+  // bool isPalindrome = true;
+  // for (int i = 0; i < listOne.length; i++) {
+  //   if (listOne[i] != reverseList[i]) {
+  //     isPalindrome = false;
+  //     break;
+  //   }
+  // }
+
+  // if (isPalindrome) {
+  //   print('The list is a palindrome');
+  // } else {
+  //   print('The list is not a palindrome.');
+  // }
 }

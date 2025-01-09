5977e4aae4a0a38d9cd19051a86bd5d3b597af42
HassanCodeWave <shforshaikhhassan@gmail.com>
2025-01-09 23:41:24 +0500

    nested list example added

---
+++ nested_list.dart
@@ -0,0 +1,17 @@
+void main(){
+    dynamic abc = [
+    [3, 5, 6],
+    {
+      "hello": {
+        2: ["abc", 2, 3],
+        1: {
+          "1": 0, // access this 0
+          1: "Access this Line", // print(abc[1]["hello"][1][1]);
+        },
+      }
+    },
+    ["1"],
+  ];
+  print(abc[1]["hello"][1]["1"]); // access 0 of line 10
+  print(abc[1]["hello"][1][1]); // print "Access this Line" of line 11
+}

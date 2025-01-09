c239f7f71dc2a53e54cd30053810f37b217dc643
HassanCodeWave <shforshaikhhassan@gmail.com>
2025-01-09 23:43:13 +0500

    for each loop added

--- loop_in_list.dart
+++ loop_in_list.dart
@@ -1,15 +1,14 @@
 void main() {
   List<int> numbers1 = [1, 2, 3, 4, 5];
-  // forEach - print each element of the list
-  numbers1.forEach((element) {
-    print(element);
+  numbers1.forEach((index) {
+    print(index);
   });
 
   // Dart Iterating List elements
 
-  var list2 = ["Smith", "Peter", "Handscomb", "Devansh", "Cruise"];
-  print("Iterating the List Element");
-  list2.forEach((item) {
-    print("${list2.indexOf(item)}: $item");
-  });
+  // var list2 = ["Smith", "Peter", "Handscomb", "Devansh", "Cruise"];
+  // print("Iterating the List Element");
+  // list2.forEach((item) {
+  //   print("${list2.indexOf(item)}: $item");
+  // });
 }

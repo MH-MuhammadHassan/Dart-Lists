e2085c949da49aa4c19c34760730db14befa9ca9
HassanCodeWave <shforshaikhhassan@gmail.com>
2025-01-09 23:45:21 +0500

    .take and added for comparision

--- sublist_method.dart
+++ sublist_method.dart
@@ -1,6 +1,8 @@
 // sublist: Extracts a portion of the list.
 void main() {
   List<int> numbers = [1, 2, 3, 4, 5];
+  var nemm = numbers.take(3);
+  print(nemm);
   var newList = numbers.sublist(0, 3); // 0 = index and 3 = length
   print(newList); // output 1,2,3
 }
